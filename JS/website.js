aa6851cebd06e4ebe553e724490e32a4e8777bde
Soham Naigaonkar <soham.naigaonkar@gmail.com>
2022-03-31 11:10:55 +0530

    responsive 1

--- JS/website.js
+++ JS/website.js
@@ -6,6 +6,7 @@ if (width < 1000) {
     let projects = document.getElementsByClassName('project');
 
     Array.from(projects).forEach(function(element, index) {
+        element.getElementsByClassName('proj-info')[0].removeAttribute('data-aos');
         let projInfo = element.getElementsByClassName('proj-info')[0].outerHTML;
         let projImg = element.getElementsByClassName('proj-img')[0].outerHTML;
 
@@ -14,5 +15,21 @@ if (width < 1000) {
         element.innerHTML = newHTML
 
         // console.log(newHTML);
+
     })
+
+    let myinfoNameH1 = document.getElementsByClassName('my-name-info')[0].getElementsByTagName('h1')[0];
+
+    myinfoNameH1.innerText = `Soham N`;
+}
+
+var MenuItems = document.getElementById("MenuItems");
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+    if (MenuItems.style.maxHeight == "0px") {
+        MenuItems.style.maxHeight = "200px"
+    } else {
+        MenuItems.style.maxHeight = "0px"
+    }
 }
