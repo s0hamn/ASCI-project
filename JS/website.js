8583ec3d1d608685346ad56bb9d557f665357229
Soham Naigaonkar <soham.naigaonkar@gmail.com>
2022-03-31 13:09:26 +0530

    menu toggle

--- JS/website.js
+++ JS/website.js
@@ -29,8 +29,12 @@ if (width < 1000) {
 
 function menutoggle() {
     if (MenuItems.style.maxHeight == "0px") {
-        MenuItems.style.maxHeight = "200px"
+        MenuItems.style.maxHeight = "200px";
     } else {
-        MenuItems.style.maxHeight = "0px"
+        MenuItems.style.maxHeight = "0px";
     }
+}
+
+function menuitemtoggle() {
+    MenuItems.style.maxHeight = "0px";
 }
